0134c4f9b41099aeea6075e5a4e55f496072bc74
potsables <alextrottier2014@gmail.com>
2018-07-30 11:16:39 -0700

    updated comments

--- Examples/KingOfTheTezos.ml
+++ Examples/KingOfTheTezos.ml
@@ -29,16 +29,30 @@ let%entry main
   
   (* Get current amount of transaction *)
   let throne = Current.amount() in
+  (* set the passed in parameter in variable king *)
   let king = parameter in
+  (* get the address of the current transaction *)
   let king_address = Current.source() in
+  (* If they are the current king, update their throne, 
+  otherwise attempt to overtake throne *)
   if king_address = storage.king_address then
     let storage = storage.throne <- storage.throne + throne in
     ( ([] : operation list), storage)
   else
-    let storage = storage.king <- king in
-    let storage = storage.king_address <- king_address in
+    (* Calculate throne size after paying tribute *)
     let throne_minus_fee = throne - storage.fee in
-    let storage = storage.throne <- throne_minus_fee in
-    let owner = Account.default storage.owner in
-    let op = Contract.call owner storage.fee () in
-    ( [op], storage)
+    (* make sure they have a big enough throne, otherwise fail *)
+    if throne_minus_fee < storage.throne then
+      Current.failwith "throne too small after paying tribute"
+    else 
+      (* Update current king key hash *)
+      let storage = storage.king <- king in
+      (* Update current king address *)
+      let storage = storage.king_address <- king_address in
+      (* Update throne size *)
+      let storage = storage.throne <- throne_minus_fee in
+      (* Create sendable address from owner *)
+      let owner = Account.default storage.owner in
+      (* Send tribute to owner *)
+      let op = Contract.call owner storage.fee () in
+      ( [op], storage)
