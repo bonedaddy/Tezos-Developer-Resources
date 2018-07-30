f024bfdf44390caca099acdd88eb732be6d46809
potsables <alextrottier2014@gmail.com>
2018-07-30 12:26:03 -0700

    variable name change

--- Examples/KingOfTheTezos_V2.ml
+++ Examples/KingOfTheTezos_V2.ml
@@ -28,7 +28,7 @@ let%entry main
     (storage : storage) = 
   
   (* Get current amount of transaction *)
-  let throneBid = Current.amount() in
+  let throne_bid = Current.amount() in
   (* set the passed in parameter in variable king *)
   let king = parameter in
   (* get the address of the current transaction *)
@@ -36,18 +36,18 @@ let%entry main
   (* If they are the current king, update their throne, 
   otherwise attempt to overtake throne *)
   if king_address = storage.king_address then
-    let storage = storage.throne <- storage.throne + throneBid in
+    let storage = storage.throne <- storage.throne + throne_bid in
     ( ([] : operation list), storage)
   else
     (* Calculate throne size after paying tribute *)
-    let throne_minus_tribute = throneBid - storage.tribute in
-    let tribute = throneBid - throne_minus_tribute in
+    let throne_minus_tribute = throne_bid - storage.tribute in
+    let tribute = throne_bid - throne_minus_tribute in
     (* make sure they have a big enough throne, otherwise fail *)
     if throne_minus_tribute < storage.throne then
       Current.failwith "throne too small after paying tribute"
     else
-      let oldThrone = storage.throne in
-      let oldKing = storage.king in 
+      let old_throne = storage.throne in
+      let old_king = storage.king in 
       (* Update current king key hash *)
       let storage = storage.king <- king in
       (* Update current king address *)
@@ -57,7 +57,7 @@ let%entry main
       (* Create sendable address from owner *)
       let owner = Account.default storage.owner in
       (* Send tribute to owner *)
-      let oldKingTyped = Account.default oldKing in
+      let old_king_typed = Account.default old_king in
       let op1 = Contract.call owner tribute () in
-      let op2 = Contract.call oldKingTyped oldThrone () in
+      let op2 = Contract.call old_king_typed old_throne () in
       ( [op1; op2], storage)
