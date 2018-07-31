9db7c0cfe194f16e01490f784e6a1f352f9e38c7
potsables <alextrottier2014@gmail.com>
2018-07-30 17:20:29 -0700

    small logic bug

--- Examples/KingOfTheTezos/KingOfTheTezos_V4.ml
+++ Examples/KingOfTheTezos/KingOfTheTezos_V4.ml
@@ -81,7 +81,7 @@ let%entry main
       (*calculate war chest**)
       let war_chest = storage.throne - storage.initial_throne in
       (* update initial throne *)
-      let storage = storage.throne <- throne_bid_minus_tributes in
+      let storage = storage.throne <- throne_bid - storage.greetings_tribute in
       (* update players *)
       let storage = storage.players <- Map.add king throne_bid_minus_tributes storage.players in 
       (* create the creator refund *)
