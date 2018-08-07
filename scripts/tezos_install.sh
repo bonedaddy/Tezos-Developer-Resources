a6bc401c9c72a4592abeca777e6eca2a4d7feb90
potsables <alextrottier2014@gmail.com>
2018-08-06 19:28:22 -0700

    fixed bug in script

--- scripts/tezos_install.sh
+++ scripts/tezos_install.sh
@@ -9,26 +9,35 @@
 #  -> Pop!_OS 18.04
 
 ## Check for ubuntu 16.04 an dinstall bubble wrap
-DISTRIB_RELEAS=$(grep "DISTRIB_RELEASE" /etc/lsb-release | awk -F '=' '{print $2}')
-if [[ DISTRIB_RELEASE == "16.04" ]]; then
+DISTRIB_RELEASE=$(grep "DISTRIB_RELEASE" /etc/lsb-release | awk -F '=' '{print $2}')
+TEZOS_NETWORK="betanet"
+
+if [[ "$DISTRIB_RELEASE" == "16.04" ]]; then
+    echo "[INFO] ubuntu 16.04 detected"
+    echo "[INFO] Installing bubblewrap ppa"
     sudo add-apt-repository ppa:ansible/bubblewrap -y
     sudo add-apt-repository ppa:git-core/ppa -y
     sudo apt-get update -y
+    echo "[INFO] Installing 16.04 dependencies"
     sudo apt install -y libev-dev libgmp-dev pkg-config wget make gcc m4 g++ aspcud curl bzip2 rsync libhidapi-dev
-    # gett the following error on ubuntu 16.04
-    # [ERROR] The compilation of conf-hidapi failed at "/home/rtrade/.opam/opam-init/hooks/sandbox.sh build pkg-config hidapi-libusb".
 fi
 
+echo "[INFO] installing general dependencies"
 sudo apt install -y git m4 build-essential patch unzip bubblewrap wget
-wget https://github.com/ocaml/opam/releases/download/2.0.0-rc4/opam-2.-0.0-rc4-x86_64-linux
+echo "[INFO] downloading opam rc4"
+wget https://github.com/ocaml/opam/releases/download/2.0.0-rc4/opam-2.0.0-rc4-x86_64-linux
 sudo cp opam-*linux /usr/local/bin/opam
 sudo chmod a+x /usr/local/bin/opam
+echo "[INFO] downloading tezos"
 git clone https://gitlab.com/tezos/tezos.git
 cd tezos
-git checkout betanet
+echo "[INFO] checking out branch $TEZOS_NETWORK"
+git checkout "$TEZOS_NETWORK"
 opam init --bare
+echo "[INFO] building dependencies"
 make build-deps
 eval $(opam env)
+echo "[INFO] building tezos"
 make
 export PATH=~/tezos:$PATH
 source ./src/bin_client/bash-completion.sh
